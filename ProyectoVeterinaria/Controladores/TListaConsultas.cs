5da9968e7142ea6ee7ccd40d3fa23195684625b5
Sthefano Alejandro Ordoñez Castillo <sordonez16@utmachala.edu.ec>
2026-05-31 20:46:18 -0500

    Cambios

---
+++ ProyectoVeterinaria/Controladores/TListaConsultas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoVeterinaria.Controladores
+{
+    public class TListaConsultas
+    {   
+        public static List<Consultas> listaConsultas = new List<Consultas>();
+
+        public static void Agregar(Consultas consulta)
+        {
+            listaConsultas.Add(consulta);
+        }
+
+        public static void Eliminar(int index)
+        {
+            if (index >= 0 && index < listaConsultas.Count)
+            {
+                listaConsultas.RemoveAt(index);
+            }
+        }
+
+        public static void Modificar(int pos, Consultas consulta)
+        {
+            listaConsultas[pos] = consulta;
+        }
+    }
+}
+
+
+
