e3b03ce30afeba7ddbd373eafeca331f557815cf
Sthefano Alejandro Ordoñez Castillo <sordonez16@utmachala.edu.ec>
2026-05-31 20:49:31 -0500

    Cambios

---
+++ ProyectoVeterinaria/Formularios/frmEditConsultas.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace ProyectoVeterinaria.Formularios
+{
+    partial class frmEditConsultas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtpFechaNacimiento = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cmbCliente = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cmbMascota = new System.Windows.Forms.ComboBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCosto = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDiagnostico = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMotivo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdConsulta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpFechaNacimiento
+            // 
+            this.dtpFechaNacimiento.Location = new System.Drawing.Point(113, 268);
+            this.dtpFechaNacimiento.Name = "dtpFechaNacimiento";
+            this.dtpFechaNacimiento.Size = new System.Drawing.Size(135, 20);
+            this.dtpFechaNacimiento.TabIndex = 70;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(39, 274);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(60, 13);
+            this.label9.TabIndex = 69;
+            this.label9.Text = "Fecha Nac";
+            // 
+            // cmbCliente
+            // 
+            this.cmbCliente.FormattingEnabled = true;
+            this.cmbCliente.Location = new System.Drawing.Point(113, 193);
+            this.cmbCliente.Name = "cmbCliente";
+            this.cmbCliente.Size = new System.Drawing.Size(135, 21);
+            this.cmbCliente.TabIndex = 68;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(39, 196);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 67;
+            this.label7.Text = "Cliente";
+            // 
+            // cmbMascota
+            // 
+            this.cmbMascota.FormattingEnabled = true;
+            this.cmbMascota.Location = new System.Drawing.Point(113, 230);
+            this.cmbMascota.Name = "cmbMascota";
+            this.cmbMascota.Size = new System.Drawing.Size(135, 21);
+            this.cmbMascota.TabIndex = 66;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(168, 317);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(80, 23);
+            this.btnSalir.TabIndex = 65;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(42, 317);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 64;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 233);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 63;
+            this.label6.Text = "Mascota";
+            // 
+            // txtCosto
+            // 
+            this.txtCosto.Location = new System.Drawing.Point(113, 158);
+            this.txtCosto.Name = "txtCosto";
+            this.txtCosto.Size = new System.Drawing.Size(135, 20);
+            this.txtCosto.TabIndex = 62;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 161);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 61;
+            this.label4.Text = "Costo";
+            // 
+            // txtDiagnostico
+            // 
+            this.txtDiagnostico.Location = new System.Drawing.Point(113, 122);
+            this.txtDiagnostico.Name = "txtDiagnostico";
+            this.txtDiagnostico.Size = new System.Drawing.Size(135, 20);
+            this.txtDiagnostico.TabIndex = 60;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 59;
+            this.label3.Text = "Diagnostico";
+            // 
+            // txtMotivo
+            // 
+            this.txtMotivo.Location = new System.Drawing.Point(113, 86);
+            this.txtMotivo.Name = "txtMotivo";
+            this.txtMotivo.Size = new System.Drawing.Size(135, 20);
+            this.txtMotivo.TabIndex = 58;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 57;
+            this.label2.Text = "Motivo";
+            // 
+            // txtIdConsulta
+            // 
+            this.txtIdConsulta.Location = new System.Drawing.Point(113, 50);
+            this.txtIdConsulta.Name = "txtIdConsulta";
+            this.txtIdConsulta.Size = new System.Drawing.Size(135, 20);
+            this.txtIdConsulta.TabIndex = 56;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 55;
+            this.label1.Text = "IdConsulta";
+            // 
+            // frmEditConsultas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 391);
+            this.Controls.Add(this.dtpFechaNacimiento);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.cmbCliente);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbMascota);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCosto);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtDiagnostico);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMotivo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIdConsulta);
+            this.Controls.Add(this.label1);
+            this.Name = "frmEditConsultas";
+            this.Text = "frmEditConsultas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpFechaNacimiento;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ComboBox cmbCliente;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cmbMascota;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCosto;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDiagnostico;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMotivo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtIdConsulta;
+        private System.Windows.Forms.Label label1;
+    }
+}
